ca605ab3a948551027802693941dcdd98cc2dff2
Daniel Leja <wolfbiter@gmail.com>
2013-09-06 14:12:43 -0700

    huge progress, made player buttons (Start) (Stop) (Change Transition) (Transition Now), transitions are selectable. the buttons all seems to be working, but i haven't done extensive testing other than clicking around

--- client/client.js
+++ client/client.js
@@ -4,9 +4,9 @@ Transitions = new Meteor.Collection("Transitions");
 //
 // load web audio api
 //
-var context, currentSong, currTime, BUFFERS = [];
+var context, currSong, offset = 0, lastMixTime = 0, BUFFERS = [], TIMERS = [], currTransition;
 
-//try {
+try {
   window.AudioContext = window.AudioContext || window.webkitAudioContext;
   context = new AudioContext();
 
@@ -29,7 +29,7 @@ var context, currentSong, currTime, BUFFERS = [];
             return;
           }
           BUFFERS.push(buffer);
-          if (callback) callback(buffer);
+          if (callback) return callback(buffer);
           else return buffer;
         },
         function(error) {
@@ -65,6 +65,7 @@ var context, currentSong, currTime, BUFFERS = [];
     //var compressor = context.createDynamicsCompressor();
     var gain = context.createGain();
     source.buffer = buffer;
+    // TODO: make gain a variable
     gain.gain.value = 0.52;
 
     // route source
@@ -90,45 +91,80 @@ var context, currentSong, currTime, BUFFERS = [];
   }
 
 
-  function playMix(startSong, offset, iter) {
-    // offset is the position of startSong when this method gets called
-    offset = offset || 0;
-
-    console.log("offset: "+offset);
-    console.log("currTime: "+context.currentTime);
-    currTime = context.currentTime;
-    currentSong = startSong;
+  function playMix(transition) {
+    lastMixTime = context.currentTime;
+    if (transition) {
+      console.log("scheduling transition for NOW");
+    } else if (!currSong) {
+      console.log("ERROR: cannot play mix when there is no currSong");
+      return;
+    }
 
     // load songs and buffers
-    var transition = chooseTransition(startSong, offset);
+    var now = !!transition;
+    currTransition = transition = (typeof transition == 'object') ?
+      transition : chooseTransition(currSong, offset);
     var endSong = { name: transition.endSong, type: "mp3" };
 
     // async
     makeTransitionBuffer(transition, function (transitionBuffer) {
       makeSongBuffer(endSong, function (endBuffer) {
 
-        // TODO: what is with this currTime thing?!?
-        var lag = context.currentTime - currTime;
-        var songDuration = transition.startTime - offset + currTime;
+        var lag = context.currentTime - lastMixTime;
+        var songDuration = transition.startTime - offset - lag;
+        songDuration = now ? 0 : songDuration; // play immediately if scheduled now
         var transitionDuration = songDuration + transitionBuffer.duration;
 
-        console.log("iter:"+iter);
-        console.log("transition in: "+(songDuration - lag));
-        console.log("next song in: "+(transitionDuration - lag));
-        console.log("playMix lag: "+lag);
+        console.log("transition in: "+songDuration);
+        console.log("next song in: "+transitionDuration);
+        console.log("buffer load lag: "+lag);
 
-        // make a transition
-        startSong.source.stop(songDuration);
-        playBuffer(transitionBuffer, songDuration, 0);
-        endSong.source = playSongBuffer(endBuffer, transitionDuration, transition.endTime);
-
-        // continue the mix
-        setTimeout(function() {
-          playMix(endSong, transition.endTime, iter + 1);
-        }, (transitionDuration - context.currentTime) * 1000.0); // convert [s] to [ms]
+        // schedule the transition
+        scheduleTransition(function() {
+          // start the transition
+          (currSong && currSong.source.stop(0));
+          transition.source = playBuffer(transitionBuffer);
+          currSong = transition;
+        }, songDuration, function() {
+          // start the next song and continue the mix
+          endSong.source = playSongBuffer(endBuffer, 0, transition.endTime);
+          currSong = endSong;
+          offset = transition.endTime;
+          playMix();
+        }, transitionDuration);
 
       });
     });
+  }
+
+  function unscheduleTransition() {
+    // cancel pending transition
+    for (var i = 0; i < TIMERS.length; i++) {
+      clearTimeout(TIMERS[i]);
+    }
+  }
+
+  function scheduleTransition(trans, transTime, next, nextTime) {
+    unscheduleTransition(); // in case there's already one queued
+    TIMERS[0] = setTimeout(trans, transTime * 1000.0);
+    TIMERS[1] = setTimeout(next, nextTime * 1000.0);
+  }
+
+  // TODO: make it so you can only do this (and transition now) during a song, not during a transition!
+  // ^ implemented by checking currSong to see if it's a transition
+  // TODO: make it so this changes to the already scheduled song (rather than reselecting)
+  function changeTransition() {
+    console.log("CHANGING TRANSITIONS!");
+    unscheduleTransition();
+    // make a new transition
+    offset += (context.currentTime - lastMixTime);
+    playMix();
+  }
+
+  function stopMix() {
+    console.log("STOPPING EVERYTHING!");
+    unscheduleTransition();
+    (currSong && currSong.source.stop(0));
   }
 
   function shuffle(o){
@@ -137,10 +173,11 @@ var context, currentSong, currTime, BUFFERS = [];
   }
 
   // algorithm to decide which transition comes next. will use Session for settings?
-  function chooseTransition(startSong, offset) {
+  function chooseTransition(startSong, offset, callback) {
     var choices = Transitions.find({
       startSong: startSong.name,
-      startTime: { $gt: offset }
+      // TODO: formalize this concept of "5" - it's the buffer load time ("lag")
+      startTime: { $gt: offset + 5 }
     }).fetch();
 
     for (var i = 0; i < choices.length; i++) {
@@ -150,62 +187,21 @@ var context, currentSong, currTime, BUFFERS = [];
 
     console.log("CHOOSING transition: " + choice.endSong);
 
-    return choice;
+    if (callback) { return callback(choice); }
+    else return choice;
   }
 
-  setTimeout(function () {
+  function startMix(startSongName, startPos) {
+    startPos = startPos || 60;
+    startSongName = startSongName || "dont you worry child";
 
-    currTime = context.currentTime;
-    var offset = 160;
-
-    var startSong = { name: "dont you worry child", type: "mp3" };
+    var startSong = { name: startSongName, type: "mp3" };
     makeSongBuffer(startSong, function (startBuffer) {
-      startSong.source = playSongBuffer(startBuffer, 0, offset);
-      playMix(startSong, offset, 0);
+      startSong.source = playSongBuffer(startBuffer, 0, startPos);
+      currSong = startSong;
+      playMix();
     });
-  }, 3000);
-
-/*
-  makeBuffer("/songs/stars come out.mp3", function() {
-    makeBuffer("/transitions/stars come out-no beef.wav", function() {
-      makeBuffer("/songs/no beef.mp3", function() {
-        var start = BUFFERS[0];
-        var transition = BUFFERS[1];
-        var end = BUFFERS[2];
-
-        var latency = 0.628;
-        var advance = 105;
-        var songDuration = 107.125320 - latency;
-        var transitionDuration = songDuration + transition.duration;
-
-        playBuffer(start, 0, advance, songDuration - advance);
-        playBuffer(transition, songDuration - advance);
-        playBuffer(end, transitionDuration - advance, 165.663742 - latency);
-      });
-    });
-  });
-*/
-/*
-  makeBuffer("/songs/no beef.mp3", function() {
-    makeBuffer("/transitions/no beef-epic.wav", function() {
-      makeBuffer("/songs/epic.mp3", function() {
-        var start = BUFFERS[0];
-        var transition = BUFFERS[1];
-        var end = BUFFERS[2];
-
-        var latency = 0.5465;
-        var advance = 300;
-        var songDuration = 304.146332 - latency;
-        var transitionDuration = songDuration + transition.duration;
-
-        playBuffer(start, 0, advance, songDuration - advance);
-        playBuffer(transition, songDuration - advance);
-        playBuffer(end, transitionDuration - advance, 35.235523 - latency);
-      });
-    });
-  });
-*/
-
+  }
 /*
 makeBuffer("/songs/alive.mp3", function() {
     makeBuffer("/transitions/alive-torrent.wav", function() {
@@ -230,18 +226,47 @@ makeBuffer("/songs/alive.mp3", function() {
     });
   });
 */
-//}
-//catch(e) {
-//  alert('Web Audio API is not supported in this browser');
-//}
+}
+catch(e) {
+  console.log('Web Audio API is not supported in this browser');
+}
 
 //
 // do meteor stuff
 //
+Template.player.events({
+
+  'click #start': function() {
+    stopMix();
+    var transition = Transitions.findOne(Session.get("selected_transition"));
+    if (transition) playMix(transition);
+    else startMix();
+  },
+
+  'click #transitionNow': function() {
+    playMix(currTransition);
+  },
+
+  'click #changeTransition': changeTransition,
+  'click #stop': stopMix
+});
+
+Template.transition.events({
+  'click': function() {
+    Session.set("selected_transition", this._id);
+  }
+});
+
+// TODO: sliders for volume and offset. maybe a skip 30sec (or to next transition) button?
+
 Template.songs.songs = function () {
   return Songs.find();
 };
 
 Template.transitions.transitions = function () {
   return Transitions.find();
+};
+
+Template.transition.selected = function () {
+  return Session.equals("selected_transition", this._id) ? "selected" : "";
 };
