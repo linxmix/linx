443b2169a6c205ce962b913fae268e17ef14e03f
Daniel Leja <wolfbiter@gmail.com>
2013-10-11 14:45:31 -0700

    close #63, close #69

---
+++ client/views/uploader/lib/uploader.js
@@ -0,0 +1,238 @@
+//
+// init
+//
+Session.set("waves", []);
+
+// private variables
+
+var waveColors = {
+  'startWave': 'mediumorchid',
+  'transitionWave': 'steelblue',
+  'endWave': 'coral'
+};
+
+var progressColors = {
+  'startWave': 'purple',
+  'transitionWave': 'midnightblue',
+  'endWave': 'orangered'
+};
+
+var cursorColors = {
+  'startWave': 'navy',
+  'transitionWave': 'navy',
+  'endWave': 'navy'
+};
+
+Meteor.startup(function () {
+
+  //
+  // make waves
+  //
+  makeWave("startWave", "Drop starting song here.", true);
+  makeWave("transitionWave", "Drop transition here.", false);
+  makeWave("endWave", "Drop ending song here.", true);
+
+});
+
+//
+// /init
+//
+
+function makeWave(id, loadText, isSongWave) {
+  var wave = Object.create(WaveSurfer);
+  wave.id = id;
+
+  // update session object to render new template
+  var sessionWaves = Session.get("waves");
+  sessionWaves.push({
+    'id': id,
+    'loadText': loadText,
+    'isSongWave': isSongWave
+  });
+  Session.set("waves", sessionWaves);
+  Uploader.waves[id] = wave;
+
+  // hack to make mark events more precise
+  wave.bindMarks = function () {
+    var my = wave,
+        firing = false;
+    wave.backend.createScriptNode();
+    wave.backend.on('audioprocess', function () {
+      if (!firing) {
+        Object.keys(my.markers).forEach(function (id) {
+          var marker = my.markers[id];
+          var position = marker.position.toPrecision(3);
+          var time = my.backend.getCurrentTime().toPrecision(3);
+          if (position == time) {
+
+            firing = true;
+            var diff = marker.position - my.backend.getCurrentTime();
+            Meteor.setTimeout(function () {
+              console.log("firing with diff: "+diff);
+              firing = false;
+              my.fireEvent('mark', marker);
+              marker.fireEvent('reached');
+            }, (diff * 1000) - 2); // subtract 2ms to improve accuracy
+          }
+        });
+      }
+    });
+  };
+  // /hack
+
+  //
+  // flash when reaching a mark
+  //
+  var flashInterval;
+  wave.on('mark', function (marker) {
+    var markerColor = marker.color;
+
+    function flashMarker() {
+      marker.update({ color: 'yellow' });
+        Meteor.setTimeout(function () {
+          marker.update({ color: markerColor });
+        }, 200);
+    }
+
+    // only flash if we aren't already flashing
+    if (!flashInterval) {
+      // flash now
+      flashMarker();
+      // set flash interval
+      flashInterval = Meteor.setInterval(flashMarker, 400);
+      // end flash interval
+      Meteor.setTimeout(function() {
+        Meteor.clearInterval(flashInterval);
+        flashInterval = undefined;
+        marker.update({ color: markerColor });
+      }, 800);
+    }
+
+  });
+}
+
+Template.wave.rendered = function () {
+  var id = this.data.id;
+
+  // first handle id-specific stuff
+  var wave = Uploader.waves[id];
+  switch (id) {
+
+    // when a mark is reached, pause this and play transitionWave
+    case 'startWave':
+    wave.on('mark', function(mark) {
+      if (mark.id === 'end') {
+        var transitionWave = Uploader.waves['transitionWave'];
+        var transitionStart =
+          (transitionWave.markers['start'].position) /
+          getWaveDuration(transitionWave);
+        transitionWave.seekTo(transitionStart);
+        playWave(transitionWave);
+      }
+    }); break;
+
+    // when a mark is reached, pause this and play endWave
+    case 'transitionWave':
+    wave.on('mark', function(mark) {
+      if (mark.id === 'end') {
+        var endWave = Uploader.waves['endWave'];
+        var endStart =
+          (endWave.markers['start'].position) /
+          getWaveDuration(endWave);
+        endWave.seekTo(endStart);
+        playWave(endWave);
+      }
+    }); break;
+
+  }
+
+  // init wave
+  var selector = '#'+id;
+  wave.init({
+    'container': $(selector+' .waveform')[0],
+    'waveColor': waveColors[id],
+    'progressColor': progressColors[id],
+    'cursorColor': cursorColors[id],
+    'minPxPerSec': 10,
+    'fillParent': false,
+    'scrollParent': true,
+    'cursorWidth': 2,
+    'markerWidth': 2,
+    'renderer': 'Canvas',
+    'audioContext': audioContext
+  });
+  wave.bindDragNDrop($(selector+' .waveform')[0]);
+  wave.bindMarks();
+
+  // TODO: progress bar after wavesurfer issue is fixed
+  wave.on('ready', function() {
+    $(selector+' .loadText').hide();
+
+    // if wave has no song, it must been drag and drop
+    // => so prompt the user to get the metadata
+    if (!wave.song && (id !== 'transitionWave')) {
+      $('#songInfoDialog').modal('show');
+      Uploader.waves['modalWaveOpen'] = Uploader.waves[id];
+    }
+
+  });
+};
+
+Template.uploaderPage.rendered = function () {
+  // initialize tooltips
+  $('.helptip').tooltip();
+};
+
+Template.uploader.waves = function () {
+  return Session.get("waves");
+};
+
+//
+// Make Uploader
+//
+
+Uploader = {
+
+  //
+  // vars
+  //
+  'waves': {},
+
+  //
+  // methods
+  //
+
+  // position is in seconds
+  'getWavePosition': function(wave) {
+    return wave.timings()[0];
+  },
+
+  // duration is in seconds
+  'getWaveDuration': function(wave) {
+    return wave.timings()[1];
+  },
+
+  // progress is in percent
+  'getWaveProgress': function(wave) {
+    return getWavePosition(wave) / getWaveDuration(wave);
+  },
+
+  'playWave': function(wave) {
+    // curry arg
+    if (typeof wave !== 'object') { wave = Uploader.waves[wave]; }
+    Uploader.pause();
+    if (wave) {
+      Uploader.waves['playingWave'] = wave;
+      wave.play();
+    }
+  },
+
+  'pause': function() {
+    var playingWave = Uploader.waves['playingWave'];
+    if (playingWave) {
+      playingWave.pause();
+      Uploader.waves['playingWave'] = undefined;
+    }
+  }
+
+};
--- client/views/uploader/mousebindings.js
+++ client/views/uploader/mousebindings.js
@@ -1,271 +1,3 @@
-Session.set("waves", []);
-
-var waves = {};
-var lastWaveClicked, modalWaveOpen;
-
-var waveColors = {
-  'startWave': 'mediumorchid',
-  'transitionWave': 'steelblue',
-  'endWave': 'coral'
-};
-
-var progressColors = {
-  'startWave': 'purple',
-  'transitionWave': 'midnightblue',
-  'endWave': 'orangered'
-};
-
-var cursorColors = {
-  'startWave': 'navy',
-  'transitionWave': 'navy',
-  'endWave': 'navy'
-};
-
-Meteor.startup(function () {
-
-  //
-  // make waves
-  //
-  makeWave("startWave", "Drop starting song here.", true);
-  makeWave("transitionWave", "Drop transition here.", false);
-  makeWave("endWave", "Drop ending song here.", true);
-
-  function makeWave(id, loadText, isSongWave) {
-    var wave = Object.create(WaveSurfer);
-    wave.id = id;
-
-    // update session object to render new template
-    var sessionWaves = Session.get("waves");
-    sessionWaves.push({
-      'id': id,
-      'loadText': loadText,
-      'isSongWave': isSongWave
-    });
-    Session.set("waves", sessionWaves);
-    waves[id] = wave;
-
-    // hack to make mark events more precise
-    wave.bindMarks = function () {
-      var my = wave,
-          firing = false;
-      wave.backend.createScriptNode();
-      wave.backend.on('audioprocess', function () {
-        if (!firing) {
-          Object.keys(my.markers).forEach(function (id) {
-            var marker = my.markers[id];
-            var position = marker.position.toPrecision(3);
-            var time = my.backend.getCurrentTime().toPrecision(3);
-            if (position == time) {
-
-              firing = true;
-              var diff = marker.position - my.backend.getCurrentTime();
-              Meteor.setTimeout(function () {
-                console.log("firing with diff: "+diff);
-                firing = false;
-                my.fireEvent('mark', marker);
-                marker.fireEvent('reached');
-              }, (diff * 1000) - 2); // subtract 2ms to improve accuracy
-            }
-          });
-        }
-      });
-    };
-    // /hack
-
-    //
-    // flash when reaching a mark
-    //
-    var flashInterval;
-    wave.on('mark', function (marker) {
-      var markerColor = marker.color;
-
-      function flashMarker() {
-        marker.update({ color: 'yellow' });
-          Meteor.setTimeout(function () {
-            marker.update({ color: markerColor });
-          }, 200);
-      }
-
-      // only flash if we aren't already flashing
-      if (!flashInterval) {
-        // flash now
-        flashMarker();
-        // set flash interval
-        flashInterval = Meteor.setInterval(flashMarker, 400);
-        // end flash interval
-        Meteor.setTimeout(function() {
-          Meteor.clearInterval(flashInterval);
-          flashInterval = undefined;
-          marker.update({ color: markerColor });
-        }, 800);
-      }
-
-    });
-  }
-
-});
-
-Template.wave.rendered = function () {
-  var id = this.data.id;
-
-  // first handle id-specific stuff
-  var wave = waves[id];
-  switch (id) {
-
-    // when a mark is reached, pause this and play transitionWave
-    case 'startWave':
-    wave.on('mark', function(mark) {
-      if (mark.id === 'end') {
-        var transitionWave = waves['transitionWave'];
-        var transitionStart =
-          (transitionWave.markers['start'].position) /
-          getWaveDuration(transitionWave);
-        transitionWave.seekTo(transitionStart);
-        playWave(transitionWave);
-      }
-    }); break;
-
-    // when a mark is reached, pause this and play endWave
-    case 'transitionWave':
-    wave.on('mark', function(mark) {
-      if (mark.id === 'end') {
-        var endWave = waves['endWave'];
-        var endStart =
-          (endWave.markers['start'].position) /
-          getWaveDuration(endWave);
-        endWave.seekTo(endStart);
-        playWave(endWave);
-      }
-    }); break;
-
-  }
-
-  // init wave
-  var selector = '#'+id;
-  wave.init({
-    'container': $(selector+' .waveform')[0],
-    'waveColor': waveColors[id],
-    'progressColor': progressColors[id],
-    'cursorColor': cursorColors[id],
-    'minPxPerSec': 10,
-    'fillParent': false,
-    'scrollParent': true,
-    'cursorWidth': 2,
-    'markerWidth': 2,
-    'renderer': 'Canvas',
-    'audioContext': audioContext
-  });
-  wave.bindDragNDrop($(selector+' .waveform')[0]);
-  wave.bindMarks();
-
-  // TODO: progress bar after wavesurfer issue is fixed
-  wave.on('ready', function() {
-    $(selector+' .loadText').hide();
-
-    // if wave has no song, it must been drag and drop
-    // => so prompt the user to get the metadata
-    if (!wave.song && (id !== 'transitionWave')) {
-      $('#songInfoDialog').modal('show');
-      modalWaveOpen = waves[id];
-    }
-
-  });
-};
-
-Template.uploaderPage.rendered = function () {
-  // initialize tooltips
-  $('.helptip').tooltip();
-};
-
-Template.uploader.waves = function () {
-  return Session.get("waves");
-};
-
-// position is in seconds
-function getWavePosition(wave) {
-  return wave.timings()[0];
-}
-
-// duration is in seconds
-function getWaveDuration(wave) {
-  return wave.timings()[1];
-}
-
-// progress is in percent
-function getWaveProgress(wave) {
-  return getWavePosition(wave) / getWaveDuration(wave);
-}
-
-function redrawWave(id) {
-  var wave = waves[id];
-  // redraw wave
-  wave.drawBuffer();
-  // update with and center screen on progress
-  var progress = getWaveProgress(wave);
-  wave.drawer.updateProgress(progress);
-  wave.drawer.recenter(progress);
-  // update markers
-  for (var waveId in wave.markers) {
-    wave.drawer.addMark(wave.markers[waveId]);
-  }
-}
-
-function clampZoom(wave, zoom) {
-  var MAX_CANVAS_WIDTH = 32767; // current limit i found in google chrome
-  var maxZoom = Math.floor(MAX_CANVAS_WIDTH / getWaveDuration(wave));
-  if (zoom >= maxZoom) {
-    return maxZoom;
-  } else if (zoom <= 1) {
-    return 1;
-  } else {
-    return zoom;
-  }
-}
-
-function zoomWave(id, zoom) {
-  var wave = waves[id];
-  wave.params['minPxPerSec'] = wave.drawer.params['minPxPerSec'] =
-    clampZoom(wave, zoom + wave.params['minPxPerSec']);
-  redrawWave(id);
-}
-
-
-//
-// Play and Pause
-//
-var playingWave;
-
-function playWave(newWave) {
-  // curry arg
-  if (typeof newWave !== 'object') { newWave = waves[newWave]; }
-  var prevWave = playingWave;
-  pause();
-  // play given wave if it wasnt the one playing
-  if ((prevWave && prevWave.id) !== newWave.id) {
-    playingWave = newWave;
-    (newWave && newWave.play());
-  }
-}
-
-function pause() {
-  if (playingWave) {
-    playingWave.pause();
-    playingWave = undefined;
-  }
-}
-
-// TODO: fix this global hack, it exists so double click on song can load that song
-uploaderLoadSong = function (e) {
-  var wave = modalWaveOpen;
-  $('.close').click(); // click is here so double click on song will close modal
-  var song = Songs.findOne(Session.get("selected_song"));
-
-  if (wave && song) {
-    wave.song = song;
-    wave.load(Mixer.getSampleUrl(song));
-  }
-};
-
 //
 // mouse events
 //
@@ -273,25 +5,19 @@ Template.uploaderPage.events({
 
   // TODO: move these into button click event handler
 
-  'drop': function(e) {
-    // store raw data of any dropped files
-    waves[this.id].data = e.dataTransfer.files[0];
-    console.log(waves[this.id].data);
-  },
-
   'click #loadSong': function (e) {
     uploaderLoadSong(e);
   },
 
   'click #submitSongInfo': function (e) {
-    var wave = modalWaveOpen;
+    var wave = Uploader.waves['modalWaveOpen'];
     var serial = $('#songInfoDialog form').serializeArray();
     var name = serial[0]['value'];
     // hack to not accept empty names
     if (!name) {
       setTimeout(function () {
         $('#songInfoDialog').modal('show');
-        modalWaveOpen = wave;
+        Uploader.waves['modalWaveOpen'] = wave;
       }, 1500);
     }
     wave.song = {
@@ -309,18 +35,9 @@ Template.uploaderPage.events({
     $('#songSelectDialog').modal('hide');
     $('#songInfoDialog').modal('hide');
     setTimeout(function () {
-      modalWaveOpen = undefined;
+      Uploader.waves['modalWaveOpen'] = undefined;
     }, 1000);
   },
-
-  'click button': function (e) {
-    console.log("button clicked");
-    var action = e.target.dataset && e.target.dataset.action;
-    // pass click to event handlers
-    if (action && action in eventHandlers) {
-      eventHandlers[action](e);
-    }
-  }
 
 });
 
@@ -346,16 +63,24 @@ Template.wave.events({
   },
 
   //
-  // click
+  // area clicks
   //
   'click .songLoadText': function (e) {
     Session.set("song_select_dialog", true);
     $('#songSelectDialog').modal('show');
-    modalWaveOpen = waves[this.id];
+    Uploader.waves['modalWaveOpen'] = Uploader.waves[this.id];
   },
 
   'click .waveform': function (e) {
-    lastWaveClicked = this.id;
+    Uploader.waves['lastWaveClicked'] = this.id;
+  },
+
+  //
+  // double click
+  //
+  'dblclick .waveform': function (e) {
+    Uploader.playWave(this.id);
+    e.preventDefault();
   },
 
   //
@@ -363,7 +88,7 @@ Template.wave.events({
   //
   'mousewheel .waveform': function (e) {
     // only do this if we have a file buffer loaded
-    if (waves[this.id].backend.buffer) {
+    if (Uploader.waves[this.id].backend.buffer) {
       e.preventDefault();
       e.stopPropagation();
       var direction = e.wheelDelta >= 0 ? 1 : -1;
@@ -375,158 +100,49 @@ Template.wave.events({
     }
   },
 
-  //
-  // double click
-  //
-  'dblclick .waveform': function (e) {
-    playWave(this.id);
-    e.preventDefault();
-  },
-
 });
 
-//
-// key events
-//
-var keyBindingsInterval = Meteor.setInterval(function(){
-   if (Meteor.Keybindings) {
-     Meteor.clearInterval(keyBindingsInterval);
-     addKeyBindings();
-   }
-}, 500);
-
-function addKeyBindings() {
-  Meteor.Keybindings.add({
-    'space': eventHandlers['playPause'],
-    'left': eventHandlers['markStart'],
-    'right': eventHandlers['markEnd'],
-    'down/shift+down': eventHandlers['back'],
-    'up/shift+up': eventHandlers['forth']
-  });
+function redrawWave(id) {
+  var wave = Uploader.waves[id];
+  // redraw wave
+  wave.drawBuffer();
+  // update with and center screen on progress
+  var progress = getWaveProgress(wave);
+  wave.drawer.updateProgress(progress);
+  wave.drawer.recenter(progress);
+  // update markers
+  for (var waveId in wave.markers) {
+    wave.drawer.addMark(wave.markers[waveId]);
+  }
 }
 
-//
-// event handlers
-//
-var eventHandlers = {
+function clampZoom(wave, zoom) {
+  var MAX_CANVAS_WIDTH = 32767; // current limit i found in google chrome
+  var maxZoom = Math.floor(MAX_CANVAS_WIDTH / getWaveDuration(wave));
+  if (zoom >= maxZoom) {
+    return maxZoom;
+  } else if (zoom <= 1) {
+    return 1;
+  } else {
+    return zoom;
+  }
+}
 
-  'playPause': function(e) {
-    console.log("playpause id: "+e);
-    console.log("modalWaveOpen: "+modalWaveOpen);
-    if (!modalWaveOpen) {
-      e.preventDefault();
-      // we want spacebar to be a universal pause
-      if (playingWave) {
-        pause();
-      } else {
-        playWave(lastWaveClicked);
-      }
-    }
-  },
+function zoomWave(id, zoom) {
+  var wave = Uploader.waves[id];
+  wave.params['minPxPerSec'] = wave.drawer.params['minPxPerSec'] =
+    clampZoom(wave, zoom + wave.params['minPxPerSec']);
+  redrawWave(id);
+}
 
-  'markStart': function(e) {
-    e.preventDefault();
-    waves[lastWaveClicked].mark({
-      id: 'start',
-      color: 'rgba(0, 255, 0, 0.8)'
-    });
-  },
+// TODO: fix this global hack, it exists so double click on song can load that song
+uploaderLoadSong = function (e) {
+  var wave = Uploader.waves['modalWaveOpen'];
+  $('.close').click(); // click is here so double click on song will close modal
+  var song = Songs.findOne(Session.get("selected_song"));
 
-  'markEnd': function(e) {
-    e.preventDefault();
-    waves[lastWaveClicked].mark({
-      id: 'end',
-      color: 'rgba(255, 0, 0, 0.8)'
-    });
-  },
-
-  'back': function(e) {
-    e.preventDefault();
-    var dist = -0.005;
-    if (e.shiftKey) {
-       dist *= 50;
-    }
-    waves[lastWaveClicked].skip(dist);
-  },
-
-  'forth': function(e) {
-    e.preventDefault();
-    var dist = 0.005;
-    if (e.shiftKey) {
-       dist *= 50;
-    }
-    waves[lastWaveClicked].skip(dist);
-  },
-
-  'upload': function(e) {
-
-    // userId check
-    if (!Meteor.userId()) {
-      return alert("Sorry, but you must be logged in to submit a transition!");
-    }
-
-    // validation check
-    // TODO: add marker checks to this validation!
-    //for (var waveId in waves) {
-    //  if (!waves[waveId].backend.buffer) {
-    //    return alert("All three waves must be loaded and marked before submitting.");
-    //  }
-    //}
-
-    // upload any new songs and upload transition
-    var startWave = waves['startWave'];
-    var transitionWave = waves['transitionWave'];
-    var endWave = waves['endWave'];
-    if (startWave.song.isNew) {
-      uploadSongWave(startWave);
-    }
-    if (endWave.song.isNew) {
-      uploadSongWave(endWave);
-    }
-    uploadTransitionWave(transitionWave, startWave, endWave);
+  if (wave && song) {
+    wave.song = song;
+    wave.load(Mixer.getSampleUrl(song));
   }
 };
-
-function uploadTransitionWave(transitionWave, startWave, endWave) {
-  var startSongEnd = startWave.markers['end'].position;
-  var endSongStart = endWave.markers['start'].position;
-
-  var startTime = transitionWave.markers['start'].position;
-  var endTime = transitionWave.markers['end'].position;
-
-  var startSong = Songs.findOne({ 'name': startWave.song.name });
-  var endSong = Songs.findOne({ 'name': endWave.song.name });
-
-  // add transition to database
-  var transition = {
-    'type': 'transition',
-    'transitionType': 'active',
-    // TODO: make this based on given buffer's file name extension
-    'fileType': 'mp3',
-    'startSong': startSong._id,
-    'startSongEnd': startSongEnd,
-    'endSong': endSong._id,
-    'endSongStart': endSongStart,
-    'startTime': startTime,
-    'endTime': endTime
-  };
-  //Transitions.insert(transition);
-
-  // upload transition to s3 server
-  var url = Mixer.getSampleUrl(transition);
-  uploadWave(transitionWave, url);
-}
-
-function uploadSongWave(wave) {
-  // add song to database
-  //Songs.insert(wave.song);
-  // upload song to s3 server
-  var url = Mixer.getSampleUrl(wave.song);
-  uploadWave(wave, url);
-}
-
-// uploads buffer of given wave to given s3 url
-function uploadWave(wave, url) {
-  console.log(wave.bufferData);
-  //Meteor.call('putStream', wave.bufferData, 'test.mp3');
-}
