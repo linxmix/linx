b9a1e303bddbbe8ac61550844a5ae4a10f327ef5
Daniel Leja <wolfbiter@gmail.com>
2013-11-12 14:56:47 -0800

    /*! linx 0.0.0 2013-11-12 */

--- js/d59504d0.templates.cache.js
+++ js/d59504d0.templates.cache.js
@@ -1,14 +1,16 @@
-require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({"nbEHjA":[function(require,module,exports){
+require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({"gvSAuZ":[function(require,module,exports){
 var global=typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {};(function browserifyShim(module, exports, define, browserify_shim__define__module__export__) {
 this["JST"] = this["JST"] || {};
 
 this["JST"]["index"] = function(obj) {obj || (obj = {});var __t, __p = '', __e = _.escape;with (obj) {__p += '<!DOCTYPE html>\n<html lang="en">\n<head>\n  <link rel="stylesheet" href="css/index.css">\n</head>\n<body>\n\n  <header id="header"></header>\n  <section id="player"></section>\n  <section id="library"></section>\n  <footer id="footer"></footer>\n\n  <div id="emptyWaves"></div>\n\n  <script src="js/vendor.js"></script>\n  <script src="js/templates.js"></script>\n  <script src="js/app.js"></script>\n\n</body>\n</html>';}return __p};
 
-this["JST"]["library/Library"] = function(obj) {obj || (obj = {});var __t, __p = '', __e = _.escape;with (obj) {__p += '<div class="samples"></div>';}return __p};
+this["JST"]["library/Library"] = function(obj) {obj || (obj = {});var __t, __p = '', __e = _.escape;with (obj) {__p += '<div class="samples"></div>\n<div class="transitions"></div>';}return __p};
 
 this["JST"]["library/samples/Sample"] = function(obj) {obj || (obj = {});var __t, __p = '', __e = _.escape;with (obj) {__p += '<div class="view">\n  <label>' +((__t = ( name )) == null ? '' : __t) +'</label>\n  <button class="destroy">x</button>\n  <button class="queue">+</button>\n</div>\n<input class="edit" value="' +((__t = ( name )) == null ? '' : __t) +'">';}return __p};
 
-this["JST"]["library/samples/SampleList"] = function(obj) {obj || (obj = {});var __t, __p = '', __e = _.escape;with (obj) {__p += '<div>\n  <p>add a new sample via drag n\' drop!</p>\n  <div class="sampleDrop"></div>\n</div>\n<ul class="sampleList"></ul>\n';}return __p};
+this["JST"]["library/samples/SampleList"] = function(obj) {obj || (obj = {});var __t, __p = '', __e = _.escape;with (obj) {__p += '<div>\n  <p>add a new sample via drag n\' drop!</p>\n  <div class="drop"></div>\n</div>\n<ul class="list"></ul>\n';}return __p};
+
+this["JST"]["library/samples/TransitionList"] = function(obj) {obj || (obj = {});var __t, __p = '', __e = _.escape;with (obj) {__p += '<div>\n  <p>add a new transition via drag n\' drop!</p>\n  <div class="drop"></div>\n</div>\n<ul class="list"></ul>\n';}return __p};
 
 this["JST"]["players/Player"] = function(obj) {obj || (obj = {});var __t, __p = '', __e = _.escape;with (obj) {__p += '<ul class="tracks"></ul>';}return __p};
 
@@ -30,6 +32,6 @@ this["JST"]["players/tracks/TrackList"] = function(obj) {obj || (obj = {});var _
 }).call(global, undefined, undefined, undefined, function defineExport(ex) { module.exports = ex; });
 
 },{}],"templates":[function(require,module,exports){
-module.exports=require('nbEHjA');
-},{}]},{},["nbEHjA"])
+module.exports=require('gvSAuZ');
+},{}]},{},["gvSAuZ"])
 ;
