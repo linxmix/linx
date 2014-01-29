3e8a5f1febe082575437e9b9ed928df58af57300
Daniel Leja <wolfbiter@gmail.com>
2014-01-29 11:22:41 -0800

    close #251

--- client/views/uploader/uploaderWelcome/uploaderWelcome.js
+++ client/views/uploader/uploaderWelcome/uploaderWelcome.js
@@ -46,18 +46,32 @@ function parseFiles(files) {
 
         // mark waves
         startWave.once('ready', function () {
-          console.log("marking start end", cuePositions.outs[0]);
-          Uploader.markWaveEnd(startWave, cuePositions.outs[0]);
+          var traktor = cuePositions.times[0];
+          var webaudio = Wave.getDuration(startWave);
+          var written = cuePositions.outs[0];
+          var time = written - (traktor - webaudio);
+          console.log("marking start end", time);
+          Uploader.markWaveEnd(startWave, time);
         });
         transitionWave.once('ready', function () {
-          console.log("marking transition start", cuePositions.ins[0]);
-          console.log("marking transition end", cuePositions.outs[1]);
-          Wave.markStart(transitionWave, cuePositions.ins[0]);
-          Uploader.markWaveEnd(transitionWave, cuePositions.outs[1]);
+          var traktor = cuePositions.times[1];
+          var webaudio = Wave.getDuration(transitionWave);
+          var written_in = cuePositions.ins[0];
+          var written_out = cuePositions.outs[1];
+          var time_in = written_in - (traktor - webaudio);
+          var time_out = written_out - (traktor - webaudio);
+          console.log("marking transition start", time_in);
+          console.log("marking transition end", time_out);
+          Wave.markStart(transitionWave, time_in);
+          Uploader.markWaveEnd(transitionWave, time_out);
         });
         endWave.once('ready', function () {
-          console.log("marking end start", cuePositions.ins[1]);
-          Wave.markStart(endWave, cuePositions.ins[1]);
+          var traktor = cuePositions.times[2];
+          var webaudio = Wave.getDuration(endWave);
+          var written = cuePositions.ins[1];
+          var time = written - (traktor - webaudio);
+          console.log("marking end start", time);
+          Wave.markStart(endWave, time);
         });
 
         // load waves
@@ -83,30 +97,40 @@ function parseFiles(files) {
 
 // extract cue information
 function parseNML(text) {
-  var inCue = /NAME=\"linx in\".*\n/gi;
-  var outCue = /NAME=\"linx out\".*\n/gi;
+  var inCue = /NAME="linx in".*\n/gi;
+  var outCue = /NAME="linx out".*\n/gi;
+  var cuePos = /START="\d+\.?\d*"/;
+  var playTime = /PLAYTIME_FLOAT="\d+\.?\d*"/g;
 
   // get lines with cue data
   var inLines = text.match(inCue);
   var outLines = text.match(outCue);
+  var timeLines = text.match(playTime);
 
   // get cue positions from cue data
   var ins = inLines.map(function (line) {
-    var str = line.match(/START="\d+\.?\d*"/)[0];
+    var str = line.match(cuePos)[0];
     str = str.replace("START=", "");
     str = str.replace("\"", "");
     return parseFloat(str) / 1000.0;
   });
   var outs = outLines.map(function (line) {
-    var str = line.match(/START="\d+\.?\d*"/)[0];
+    var str = line.match(cuePos)[0];
     str = str.replace("START=", "");
     str = str.replace("\"", "");
     return parseFloat(str) / 1000.0;
   });
+  var times = timeLines.map(function (line) {
+    var str = line.replace("PLAYTIME_FLOAT=", "");
+    str = str.replace("\"", "");
+    return parseFloat(str);
+  });
   console.log(ins);
   console.log(outs);
+  console.log(times);
   return {
     'ins': ins,
     'outs': outs,
+    'times': times,
   };
 }
