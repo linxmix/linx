3b98c4db158e6bd8ee557055d3ead6781a36f5e9
Michael Williams <dinosaur@riseup.net>
2014-03-18 10:57:50 -0700

    change api and add scaffolding for other edge queries

--- server/routes/edges.js
+++ server/routes/edges.js
@@ -1,12 +1,25 @@
 module.exports = function (app, db) {
   
-  app.get("/out/:id", function (req, res, next) {
+  // read collection
+  app.get("/tracks/:trackId/out", function (req, res, next) {
 
-    var id = req.params.id;
+    var trackId = req.params.trackId;
       
-    db.relationships(id, 'out', 'connects', function (err, result) {
+    db.relationships(trackId, 'out', 'connects', function (err, result) {
       if (err) { return next(err); }
       res.json(result);
     });
   });
+
+  // create model
+  app.post("/tracks/:trackId/out", function (req, res, next) {});
+
+  // read model
+  app.get("/tracks/:trackId/out/:edgeId", function (req, res, next) {});
+
+  // update model
+  app.put("/tracks/:trackId/out/:edgeId", function (req, res, next) {});
+
+  // delete model
+  app.delete("/tracks/:trackId/out/:edgeId", function (req, res, next) {});
 };
