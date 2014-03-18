86e4902e823f47490c16df393a7855961b95a797
Daniel Leja <wolfbiter@gmail.com>
2014-03-18 14:28:38 -0700

    added quick todo to router

--- client/Router.js
+++ client/Router.js
@@ -5,6 +5,8 @@ var App = require('./views/App');
 var app = new App();
 
 module.exports = Backbone.Router.extend({
+  // TODO: add routes to specific tab pages
+  //       => (init app with a page based on the url)
   routes: {
     "*path": "default",
   },
