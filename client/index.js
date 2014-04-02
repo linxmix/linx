0751e5d769666f92ec521ab6b4e8a10e1d2a6d22
Daniel Leja <wolfbiter@gmail.com>
2014-04-02 11:18:11 -0700

    moved echojs stuff from client/index.js into client/views/pages/Search.js - everything is currently working except for echojs

--- client/index.js
+++ client/index.js
@@ -11,25 +11,10 @@ var $ = Backbone.$ = require('jquery');
 var config = require('./config');
 var Router = require('./Router');
 
-// why does this not work?
-
-/*
-var echo = echojs({
-  key: config.echoId,
-});
-
-echo('song/search').get({
-  artist: 'radiohead',
-  title: 'karma police'
-}, function (err, json) {
-  console.log(json.response);
-});
-
 SC.initialize({
   client_id: config.clientId,
   redirect_uri: config.redirectUri,
 });
-*/
 
 $(function () {
   new Router();
--- client/views/pages/Search.js
+++ client/views/pages/Search.js
@@ -3,11 +3,28 @@ var React = require('react');
 var debug = require('debug')('views:Search');
 var ReactBackboneMixin = require('backbone-react-component').mixin;
 
+var config = require('../../config');
+var echojs = require('echojs');
+
 module.exports = React.createClass({
   
   mixins: [ReactBackboneMixin],
 
   render: function () {
+
+    /* UNCOMMENTING THIS SECTION CAUSES ERROR when loading searchpage
+    var echo = echojs({
+      key: config.echoId,
+    });
+
+    echo('song/search').get({
+      artist: 'radiohead',
+      title: 'karma police'
+    }, function (err, json) {
+      console.log(json.response);
+    });
+    */
+
     return (
       <div className="ui grid">
         <div className="sixteen wide column">
