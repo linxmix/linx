9d7e26c266797a211128ffa496453a2df13b131e
Michael Williams <dinosaur@riseup.net>
2014-05-18 16:19:36 -0700

    change client to port 4000

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   clientId: "977ed530a2104a95eaa87f26fa710941", // soundcloud
-  redirectUri: "http://localhost:9966/callback.html",
+  redirectUri: "http://localhost:4000/callback.html",
   echoApiKey: "CWBME38JDGQNEJPXT",
   
   widgetModel: "Wave",
