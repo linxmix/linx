9c014fb8285b335017942b5087a593d42b89edaa
Michael Williams <dinosaur@riseup.net>
2014-05-18 16:23:49 -0700

    add production config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,16 @@
+var isProd = require('./utils').isProduction;
+
 module.exports = {
-  clientId: "977ed530a2104a95eaa87f26fa710941", // soundcloud
-  redirectUri: "http://localhost:4000/callback.html",
+
+  // soundcloud
+  clientId: isProd ?
+    "9df28962ed6bfc393d16ca291760fbc2" :
+    "977ed530a2104a95eaa87f26fa710941",
+  redirectUri: isProd ?
+    "http://linx.dj" :
+    "http://localhost:4000/callback.html",
+
+  // echonest
   echoApiKey: "CWBME38JDGQNEJPXT",
   
   widgetModel: "Wave",
