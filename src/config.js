3373508ef28e07bc15a3541ad14a58d31f904078
Michael Williams <dinosaur@riseup.net>
2014-05-18 16:25:50 -0700

    minor fix to prod redirectUri

--- src/config.js
+++ src/config.js
@@ -7,7 +7,7 @@ module.exports = {
     "9df28962ed6bfc393d16ca291760fbc2" :
     "977ed530a2104a95eaa87f26fa710941",
   redirectUri: isProd ?
-    "http://linx.dj" :
+    "http://linx.dj/callback.html" :
     "http://localhost:4000/callback.html",
 
   // echonest
