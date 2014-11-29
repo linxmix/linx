40ee6be22c342df1a85bec62066085d82cede9fb
Daniel Leja <wolfbiter@gmail.com>
2014-11-29 12:37:20 -0800

    can now load tracks into track_wave

--- client/Song/Track_Wave.js
+++ client/Song/Track_Wave.js
@@ -1,6 +1,23 @@
 Template.Track_Wave.created = function() {
   this.file = new ReactiveVar;
-  console.log(this);
+  this.wave = Object.create(WaveSurfer);
+  initWave.call(this);
+  this.loadFile = Deps.autorun(loadFile.bind(this));
+}
+
+Template.Track_Wave.rendered = function() {
+  // this.$('.wave').hide();
+  this.$('.progress-bar').hide();
+  this.wave.init({
+    container: this.$('.wave')[0],
+    waveColor: 'violet',
+    progressColor: 'purple',
+    minPxPerSec: 10,
+    height: 197,
+    fillParent: true,
+    cursorWidth: 2,
+    renderer: 'Canvas',
+  });
 }
 
 Template.Track_Wave.helpers({
@@ -11,4 +28,41 @@ Template.Track_Wave.helpers({
   file: function() {
     return Template.instance().file;
   }
-})
+});
+
+function initWave() {
+  var self = this;
+
+  this.wave.on('loading', function(percent, xhr) {
+    self.$('.progress-bar').show();
+    console.log("loading", percent);
+    self.$('.progress-bar .bar').css({ 'width': percent + '%' });
+  });
+
+  this.wave.on('ready', function() {
+    console.log('ready')
+    self.$('.progress-bar').hide();
+    self.$('.wave').show();
+  });
+}
+
+function loadFile() {
+  var file = this.file.get();
+
+  if (file) {
+
+    // hack to access the ArrayBuffer of audio data as it's read
+    this.wave.loadArrayBuffer = function(arraybuffer) {
+      var my = this;
+      this.backend.decodeArrayBuffer(arraybuffer, function (data) {
+        my.loadDecodedBuffer(data);
+        my.arrayBuffer = data;
+      }, function () {
+        my.fireEvent('error', 'Error decoding audiobuffer');
+      });
+    };
+    // /hack
+
+    this.wave.loadBlob(file);
+  }
+}
