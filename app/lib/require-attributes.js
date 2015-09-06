e07ce2cfbe2c1c5ca941a01220d7d1691dc26da3
wolfbiter <wolfbiter@gmail.com>
2015-09-06 12:54:41 -0700

    updated require-attributes tests, moved mixin tests to utils

--- app/lib/require-attributes.js
+++ app/lib/require-attributes.js
@@ -1,24 +1,26 @@
 // Creates a mixin which asserts attributes exist upon initialization
 // Usage:
 
-// import RequireAttributes from ‘/lib/require-attributes’;
+// import RequireAttributes from ‘linx/lib/require-attributes’;
 
 // export default Ember.Component.extend(
-//   RequireAttributes(‘name’, ‘value’), {
-
+//   RequireAttributes(‘name’, 'model'), {
+//
+//   somePropertyThatNeedsModel: function() {
+//     return this.get('model.someProperty');
+//   }.property('value')
 // });
 
 import Ember from 'ember';
 
-export default function() {
-  var attributes = Array.prototype.slice.call(arguments);
-
+export default function(...attributes) {
   return Ember.Mixin.create({
-    _assertAttributes: function() {
+    _assertRequiredAttributes: function() {
       attributes.forEach((attribute) => {
-        Ember.assert('Must specify a ' + attribute + ' to use ' + this.get('constructor'), !Ember.isNone(this.get(attribute)));
+        Ember.assert('Must specify a ' + attribute + ' when creating a ' + this.get('constructor'),
+          !Ember.isNone(this.get(attribute)));
       });
       this._super.apply(this, arguments);
-    }.on('init').on('ready').on('didInsertElement'),
+    }.on('init')
   });
 }
---
+++ tests/unit/utils/require-attributes-test.js
@@ -0,0 +1,61 @@
+import Ember from 'ember';
+import {
+  describe,
+  it
+} from 'mocha';
+import { expect } from 'chai';
+
+import RequireAttributes from 'linx/lib/require-attributes';
+
+function DescribeRequiredAttributesBehaviours(BaseConstructor) {
+  describe(BaseConstructor.toString(), function() {
+    let Constructor;
+
+    it('fails if missing required attributes', function() {
+      Constructor = BaseConstructor.extend(
+        RequireAttributes('a', 'b'));
+
+      expect(() => { Constructor.create(); }).to.throw();
+    });
+
+    it('fails if missing a required attribute', function() {
+      Constructor = BaseConstructor.extend(
+        RequireAttributes('a', 'b'));
+
+      expect(() => { Constructor.create({ a: true }); }).to.throw();
+    });
+
+    it('is ok for falsy but defined attributes', function() {
+      Constructor = BaseConstructor.extend(
+        RequireAttributes('a', 'b', 'c', 'd', 'e'));
+
+      expect(() => { Constructor.create({ a: '', b: false, c: [], d: {}, e: 0 }); }).not.to.throw();
+    });
+
+    it('proceeds if not missing required attributes', function() {
+      Constructor = BaseConstructor.extend(
+        RequireAttributes('a', 'b'));
+
+      expect(() => { Constructor.create({ a: true, b: true }); }).not.to.throw();
+    });
+
+    it('inherits parent requirements', function() {
+      let ParentConstructor = BaseConstructor.extend(
+        RequireAttributes('a', 'b'));
+
+      Constructor = ParentConstructor.extend(
+        RequireAttributes('c'));
+
+      expect(() => { Constructor.create({ a: true }); }).to.throw();
+      expect(() => { Constructor.create({ c: true }); }).to.throw();
+      expect(() => { Constructor.create({ a: true, b: true, c: true }); }).not.to.throw();
+    });
+
+  });
+}
+
+describe('RequireAttributes', function() {
+  DescribeRequiredAttributesBehaviours(Ember.Component);
+  DescribeRequiredAttributesBehaviours(Ember.Controller);
+  DescribeRequiredAttributesBehaviours(Ember.Object);
+});
