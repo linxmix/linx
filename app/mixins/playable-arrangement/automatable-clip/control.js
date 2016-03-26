771528f49b7c0f4ec3b1814eb65023d67a6768e5
wolfbiter <wolfbiter@gmail.com>
2016-03-25 18:31:47 -0700

    WIP: controls update value on seek

--- app/mixins/playable-arrangement/automatable-clip/control.js
+++ app/mixins/playable-arrangement/automatable-clip/control.js
@@ -1,5 +1,7 @@
 import Ember from 'ember';
 
+import RequireAttributes from 'linx/lib/require-attributes';
+
 import { isValidNumber } from 'linx/lib/utils';
 
 export const CONTROL_TYPE_GAIN = 'gain';
@@ -12,7 +14,9 @@ export const CONTROL_TYPES = [
 // provides setValueCurveAtTime
 // audioParamPath is a path to a Web Audio AudioParam
 export default function(audioParamPath) {
-  return Ember.Mixin.create({
+  return Ember.Mixin.create(
+    RequireAttributes('clip'), {
+
     audioParamPath,
 
     // required params
@@ -22,11 +26,40 @@ export default function(audioParamPath) {
     // optional params
     description: '',
     isSuspended: false,
-    value: null,
-    defaultValue: null,
+
+    _initClipListeners: Ember.on('init', function() {
+      const clip = this.get('clip');
+      clip && clip.on('schedule', this, this.updateValue);
+      clip && clip.on('unschedule', this, this.updateValue);
+    }),
+
+    _removeClipListeners: Ember.on('willDestroy', function() {
+      const clip = this.get('clip');
+      clip && clip.off('schedule', this, this.updateValue);
+      clip && clip.off('unschedule', this, this.updateValue);
+    }),
+
+    // on seek, update underlying web audio param to current value
+    updateValue() {
+      const currentTime = this.get('clip').getAbsoluteTime();
+      const automationsHash = this.get('automations');
+
+      // TODO:
+      // if currentTime in automation, leave as it
+      // if currentTime outside automation, set value to last value of most recently finished automation
+      let firstValue, lastValue;
+      const autom
+      automationsHash.forEach((automation) => {
+
+      });
+    },
 
     audioParam: Ember.computed.reads(`clip.${audioParamPath}`),
     automations: Ember.computed(() => Ember.Map.create()),
+
+    timeSort: ['startTime:asc'],
+    timeSortedAutomations: Ember.computed.sort('automations', 'timeSort'),
+
 
     addAutomation(automation, params = {}) {
       const automations = this.get('automations');
@@ -38,7 +71,6 @@ export default function(audioParamPath) {
       if (isNewAutomation) {
         // Ember.Logger.log('add new automation', params);
         this.setValueCurveAtTime(params);
-
 
       // NOTE: web audio api audio params cannot cancel individual automations.
       //       so to update automations, we must cancel and reschedule them all
