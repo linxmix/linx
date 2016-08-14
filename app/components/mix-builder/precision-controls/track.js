903fc5790178086d23168ef63594e02278efb6ab
wolfbiter <wolfbiter@gmail.com>
2016-08-14 13:17:13 -0700

    WIP: 'move transition' feature

--- app/components/mix-builder/precision-controls/track.js
+++ app/components/mix-builder/precision-controls/track.js
@@ -5,10 +5,13 @@ export default Ember.Component.extend({
 
   // required params
   clip: null,
+  isFromTrackClip: false,
+  isToTrackClip: false,
 
   // optional params
   jumpTrackTask: null,
   jumpTrack: Ember.K,
+  quantizeBeat: Ember.K,
 
   track: Ember.computed.reads('clip.track'),
 
@@ -16,6 +19,20 @@ export default Ember.Component.extend({
     analyzeTrack() {
       const analyzeTask = this.get('track.analyzeTask');
       analyzeTask.perform();
+    },
+
+    moveTransition() {
+      Ember.RSVP.resolve(this.get('clip')).then((clip) => {
+        const beat = this.get('clip.arrangement.metronome.seekBeat');
+
+        if (this.get('isFromTrackClip')) {
+          clip.set('audioEndTime', clip.getAudioTimeFromArrangementBeat(beat));
+        } else {
+          const transitionBeatCount = this.get('clip.mixItem.transition.beatCount');
+          const time = clip.getAudioTimeFromArrangementBeat(beat - transitionBeatCount);
+          clip.set('audioStartTime', time);
+        }
+      });
     }
   }
 });
