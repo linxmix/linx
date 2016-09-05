51fb49fea9c47b385368f78c7f8c5f3a927d57eb
wolfbiter <wolfbiter@gmail.com>
2016-09-04 18:51:27 -0700

    convert subheader to mix duration

--- app/components/mix-builder/header.js
+++ app/components/mix-builder/header.js
@@ -1,5 +1,7 @@
 import Ember from 'ember';
 
+import { isValidNumber } from 'linx/lib/utils';
+
 export default Ember.Component.extend({
   classNames: ['MixBuilderHeader'],
 
@@ -27,5 +29,21 @@ export default Ember.Component.extend({
   searchTracks: Ember.computed(function() {
     return this.get('store').findAll('track');
   }),
+
+  durationText: Ember.computed('mix.duration', function() {
+    const duration = this.get('mix.duration');
+    return msToTime(isValidNumber(duration) ? duration : 0);
+  }),
 });
 
+function msToTime(duration) {
+  let seconds = parseInt((duration)%60);
+  let minutes = parseInt((duration/60)%60);
+  let hours = parseInt((duration/(60*60))%24);
+
+  hours = (hours < 10) ? '0' + hours : hours;
+  minutes = (minutes < 10) ? '0' + minutes : minutes;
+  seconds = (seconds < 10) ? '0' + seconds : seconds;
+
+  return hours + 'h  ' + minutes + 'm  ' + seconds + 's';
+}
