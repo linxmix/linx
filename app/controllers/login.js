f52813541d546c46268b672c0d2e88b108a917d7
wolfbiter <wolfbiter@gmail.com>
2016-09-11 10:08:18 -0700

    add password reset

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -2,10 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   userSession: Ember.inject.service(),
+  firebaseApp: Ember.inject.service(),
 
   identification: null,
   password: null,
-  errorMessage: null,
+  message: null,
 
   actions: {
     login() {
@@ -18,12 +19,27 @@ export default Ember.Controller.extend({
         password,
       }).then(
         () => this.transitionToRoute('mixes'),
-        (error) => this.set('errorMessage', error.message)
+        (e) => this.set('message', e.message)
       );
     },
 
     logout() {
       this.get('userSession').close();
     },
+
+    sendPasswordReset() {
+      const auth = this.get('firebaseApp').auth();
+      const identification = this.get('userSession.currentUser.email') || this.get('identification');
+
+      if (!identification) {
+        this.set('message', 'Must enter an email to send password reset.');
+        return;
+      }
+
+      auth.sendPasswordResetEmail(identification).then(
+        () => this.set('message', `Successfully sent password reset to: ${identification}`),
+        (e) => this.set('message', e.message)
+      );
+    }
   },
 });
